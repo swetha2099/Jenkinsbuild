ce3e7d27d4fefb467bcdf314e1c5412cf5305a96
swetha2099 <swetha6773@gmail.com>
2021-07-29 10:01:14 +0530

    Add project files.

---
+++ Jenkinsbuild/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Jenkinsbuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 10;
+            Console.WriteLine(a);
+        }
+    }
+}
